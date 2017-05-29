eeb37f711da7a0a3f4cdc67d8e558f919eea725c
VlaSovaAnna <anja_kaa@mail.ru>
2017-05-29 09:54:05 +0300

    homeWork7 created

--- src/index.js
+++ src/index.js
@@ -1,72 +1,44 @@
-/* ДЗ 6.1 - Асинхронность и работа с сетью */
+/* ДЗ 7.1 - BOM */
 
 /**
- * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ * Функция должна создавать окно с указанным именем и размерами
  *
- * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
- * @return {Promise}
+ * @param {number} name - имя окна
+ * @param {number} width - ширина окна
+ * @param {number} height - высота окна
+ * @return {Window}
  */
-function delayPromise(seconds) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve();
-        }, seconds*1000);
-    })
+function createWindow(name, width, height) {
 }
 
 /**
- * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
- * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- * Элементы полученного массива должны быть отсортированы по имени города
+ * Функция должна закрывать указанное окно
  *
- * @return {Promise<Array<{name: String}>>}
+ * @param {Window} window - окно, размер которого надо изменить
  */
-function loadAndSortTowns() {
-    return new Promise(function(resolve, reject) {
-        // создаём запрос  
-        var XHR = ('onload' in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-        var xhr = new XHR(); 
-    
-        // настраиваем соединение
-        xhr.open('Get', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
-        // отправляем запрос
-        xhr.send();
-  
-        // обработчик события 
-        xhr.addEventListener('load', function() {
-            if (xhr.status == 200) {             
-                resolve(xhr.responseText);              
-            } else {
-                reject(xhr.statusText);
-            }
-        });
-    }).then(function(xhr) {
-        var result=JSON.parse(xhr);
-             
-        result.sort(function(el1, el2) {
-            var a = el1.name,
-                b = el2.name;
-
-            if ( a < b ) {
-                return -1;
-            } else if ( a > b ) {
-                return 1;
-            }
-
-            return 0;
-        }); 
-
-        return result;
-    }
-    , function(xhr) {
-        var error = new Error (xhr);
-
-        error.code = xhr.status;        
-        
-        return error;
-    });
+function closeWindow(window) {
 }
+
+/**
+ * Функция должна создавать cookie с указанными именем и значением
+ *
+ * @param name - имя
+ * @param value - значение
+ */
+function createCookie(name, value) {
+}
+
+/**
+ * Функция должна удалять cookie с указанным именем
+ *
+ * @param name - имя
+ */
+function deleteCookie(name) {
+}
+
 export {
-    delayPromise,
-    loadAndSortTowns
+    createWindow,
+    closeWindow,
+    createCookie,
+    deleteCookie
 };
